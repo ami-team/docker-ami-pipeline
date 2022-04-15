eb05462c42169232ebffd7e1d8fe6076c0c12a9b
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2022-04-16 01:24:20 +0200

    update

---
+++ README.md
@@ -0,0 +1,39 @@
+[![][Build Status img]][Build Status]
+[![][License img]][License]
+
+<a href="http://lpsc.in2p3.fr/" target="_blank">
+	<img src="http://ami.in2p3.fr/docs/images/logo_lpsc.png" alt="LPSC" height="72" />
+</a>
+&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="http://www.in2p3.fr/" target="_blank">
+	<img src="http://ami.in2p3.fr/docs/images/logo_in2p3.png" alt="IN2P3" height="72" />
+</a>
+&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="http://www.univ-grenoble-alpes.fr/" target="_blank">
+	<img src="http://ami.in2p3.fr/docs/images/logo_uga.png" alt="UGA" height="72" />
+</a>
+&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="http://home.cern/" target="_blank">
+	<img src="http://www.cern.ch/ami/images/logo_atlas.png" alt="CERN" height="72" />
+</a>
+&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="http://atlas.cern/" target="_blank">
+	<img src="http://ami.in2p3.fr/docs/images/logo_cern.png" alt="CERN" height="72" />
+</a>
+
+AMI Pipeline
+============
+
+Node-RED-based task pipeline.
+
+Developers
+==========
+
+* [Jérôme ODIER](https://annuaire.in2p3.fr/4121-4467/jerome-odier) ([CNRS/LPSC](http://lpsc.in2p3.fr/))
+* [Fabian Lambert](https://annuaire.in2p3.fr/3087-3350/fabian-lambert) ([CNRS/LPSC](http://lpsc.in2p3.fr/))
+
+[Build Status]:https://github.com/ami-team/docker-ami-pipeline/actions/workflows/docker-image.yml
+[Build Status img]:https://github.com/ami-team/docker-ami-pipeline/actions/workflows/docker-image.yml/badge.svg?branch=master
+
+[License]:http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.txt
+[License img]:https://img.shields.io/badge/license-CeCILL-blue.svg
---
+++ docker-entrypoint.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+########################################################################################################################
+
+sed -i "s/\/\/\s*theme\s*:.*$/page: {css: \"\/ami.css\"}, header: {image: \"\/ami.png\", title: 'pipeline'},/g" /data/settings.js
+
+sed -i "s/\/\/\s*categories\s*:\s*\[/categories: ['AMI', /g" /data/settings.js
+
+########################################################################################################################
+
+if [ ! -f /data/flows.json ]
+then
+  UUID=$(node -e 'console.log(require("crypto").randomUUID());')
+
+  cat > /data/flows.json << EOF
+[
+    {
+        "id": "7ba8d1fbe3577553",
+        "type": "tab",
+        "label": "AMI Supervisor",
+        "disabled": false,
+        "info": "",
+        "env": []
+    },
+    {
+        "id": "9f7511e86e72a8f9",
+        "type": "supervisor",
+        "z": "7ba8d1fbe3577553",
+        "name": "",
+        "topic": "${UUID}",
+        "x": 370,
+        "y": 120,
+        "wires": [
+            [
+                "247c7b8f7530815a"
+            ]
+        ]
+    },
+    {
+        "id": "bc9bcef5f75eb068",
+        "type": "mqtt in",
+        "z": "7ba8d1fbe3577553",
+        "name": "",
+        "topic": "ami/taskserver/task",
+        "qos": "2",
+        "datatype": "auto",
+        "broker": "",
+        "nl": false,
+        "rap": true,
+        "rh": 0,
+        "inputs": 0,
+        "x": 130,
+        "y": 120,
+        "wires": [
+            [
+                "9f7511e86e72a8f9"
+            ]
+        ]
+    },
+    {
+        "id": "a8b583288f8ed783",
+        "type": "mqtt in",
+        "z": "7ba8d1fbe3577553",
+        "name": "topic",
+        "topic": "${UUID}",
+        "qos": "2",
+        "datatype": "auto",
+        "broker": "",
+        "nl": false,
+        "rap": true,
+        "rh": 0,
+        "inputs": 0,
+        "x": 90,
+        "y": 190,
+        "wires": [
+            [
+                "9f7511e86e72a8f9"
+            ]
+        ]
+    },
+    {
+        "id": "cca4959c8ad7dc37",
+        "type": "comment",
+        "z": "7ba8d1fbe3577553",
+        "name": "AMI Supervisor",
+        "info": "",
+        "x": 120,
+        "y": 40,
+        "wires": []
+    },
+    {
+        "id": "247c7b8f7530815a",
+        "type": "mqtt out",
+        "z": "7ba8d1fbe3577553",
+        "name": "ami/<server_name>/command/json",
+        "topic": "",
+        "qos": "",
+        "retain": "",
+        "respTopic": "",
+        "contentType": "",
+        "userProps": "",
+        "correl": "",
+        "expiry": "",
+        "broker": "",
+        "x": 660,
+        "y": 120,
+        "wires": []
+    }
+]
+EOF
+fi
+
+########################################################################################################################
+
+npm --no-update-notifier --no-fund start --cache /data/.npm -- --userDir /data
+
+########################################################################################################################
